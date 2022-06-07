abda4912d9170ecb7e72a3839193c216f3cea02e
Matthew Nibecker <hello@mattnibecker.com>
2022-06-07 18:26:31 -0400

    Initial Commit

---
+++ term/termApp.swift
@@ -0,0 +1,17 @@
+//
+//  termApp.swift
+//  term
+//
+//  Created by Matthew Nibecker on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct termApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
